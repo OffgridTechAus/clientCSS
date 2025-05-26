8515eb47eaf5f0f8f9468cbe5d613f6fcd94bc7f
OffgridTechAus <tristan@offgridtechsolutions.com>
2025-05-26 21:41:22 +1000

    Update pwa_install_prompt.js

--- pwa_install_prompt.js
+++ pwa_install_prompt.js
@@ -5,29 +5,23 @@
  * https://github.com/ryxxn/pwa-install-prompt
  */
 
-
 // constants
 
-/**
- * !You can modify this part to change the language!
- */
 const TEXT = {
-  // common text
-  TITLE: 'Install as an app', // Install as an app
-  WAITING: 'Loading app info...', // Loading app info...
-  SKIP_BUTTON: "I'm fine, I'll just view it on my mobile.", // I'm fine, I'll just view it on my mobile.
-  INSTALL_BUTTON: 'Install as an app', // Install as an app
-  INSTALL_UNAVAILABLE: "The app is already installed or your environment doesn't support installation.", // The app is already installed or your environment doesn't support installation.
-  // IOS device specific text
+  TITLE: 'Install as an app',
+  WAITING: 'Loading app info...',
+  SKIP_BUTTON: "I'm fine, I'll just view it on my mobile.",
+  INSTALL_BUTTON: 'Install as an app',
+  INSTALL_UNAVAILABLE: "The app is already installed or your environment doesn't support installation.",
   IOS: {
-    TITLE: 'IOS App Installation Method', // IOS App Installation Method
+    TITLE: 'IOS App Installation Method',
     INSTALL_STEPS: {
-      STEP_1_1: 'Click the icon', // Click the icon
-      STEP_1_2: 'in the browser address bar.', // in the browser address bar.
-      STEP_2: "Click 'Add to Home Screen'.", // Click 'Add to Home Screen'.
+      STEP_1_1: 'Click the icon',
+      STEP_1_2: 'in the browser address bar.',
+      STEP_2: "Click 'Add to Home Screen'.",
     }
   },
-}
+};
 
 const MODAL_STYLE = `
   .wepp-modal-overlay * { box-sizing: border-box; }
@@ -42,86 +36,64 @@ const MODAL_STYLE = `
   #wepp-install-button:disabled { opacity:0.7; cursor: not-allowed; }
   #wepp-skip-button { all: initial; font:inherit; width: 100%; text-align: center; cursor: pointer; color: #a0a0a0; font-size: 14px; text-decoration: underline; }
 `;
-const IOS_MODAL_CONTENT = `
-      <div class="wepp-modal-content" style="padding: 20px;">
-        <div class="wepp-modal-body">
-          <h1 style="margin-top: 0; margin-bottom: 10px;">${TEXT.IOS.TITLE}</h1>
-          <img id="wepp-logo" alt="logo" width="64" height="64" />
-          <h2 id="wepp-name"></h2>
-        </div>
-        <ol style="margin-block:4px;padding:0;margin-left:20px;">
-          <li>
-            ${TEXT.IOS.INSTALL_STEPS.STEP_1_1}
-            <span style="vertical-align:middle;">
-              <svg
-                version="1.1"
-                viewBox="0 0 2048 2048"
-                width="24"
-                height="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  transform="translate(600,655)"
-                  d="m0 0h301l1 2v79l-1 1-284 1-17 2-10 5-6 5-6 10-3 11v751l3 12 7 10 8 6 8 3 12 2h776l54-1 12-4 9-7 6-10 3-14v-744l-2-12-5-10-5-6-14-7-4-1-18-1-278-1-1-1v-81h301l18 3 16 5 16 8 12 8 12 10v2l3 1 9 11 9 14 8 17 4 14 2 12 1 15v744l-2 21-6 21-9 19-10 14-12 13-10 8-11 7-14 7-18 6-17 3-13 1h-824l-20-2-19-5-16-7-12-7-16-13-11-12-10-15-8-16-5-17-3-18-1-42v-658l1-61 3-18 5-16 4-9 7-13 8-11 3-4h2l2-4 13-12 16-10 15-7 18-5z"
-                />
-                <path
-                  transform="translate(1023,229)"
-                  d="m0 0 5 3 269 269 1 4-53 53-2 3-4-1-172-172-1 3v753l-2 3h-81l-1-2v-758l-174 174h-3l-7-8-48-48 1-5 10-9 162-162 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 7-6 5-6 8-7z"
-                />
-              </svg>
-            </span>
-            ${TEXT.IOS.INSTALL_STEPS.STEP_1_2}
-          </li>
-          <li>
-            <p style="margin-block:4px;">${TEXT.IOS.INSTALL_STEPS.STEP_2}</p>
-          </li>
-        </ol>
 
-        <button id="wepp-skip-button">${TEXT.SKIP_BUTTON}</button>
-      </div>
-    `;
-const DEFAULT_MODAL_CONTENT = `
-    <div class="wepp-modal-content" style="padding: 20px;">
-      <div class="wepp-modal-body">
-        <h1 style="margin-top: 0; margin-bottom: 10px;">${TEXT.TITLE}</h1>
-        <img id="wepp-logo" alt="logo" width="64" height="64" />
-        <h2 id="wepp-name"></h2>
-      </div>
-      <button id="wepp-install-button" style="margin-top: 10px;padding: 10px;" disabled>${TEXT.WAITING}</button>
-      <button id="wepp-skip-button" style="margin-top: 20px">
-        ${TEXT.SKIP_BUTTON}
-      </button>
+const IOS_MODAL_CONTENT = `
+  <div class="wepp-modal-content" style="padding: 20px;">
+    <div class="wepp-modal-body">
+      <h1 style="margin-top: 0; margin-bottom: 10px;">${TEXT.IOS.TITLE}</h1>
+      <img id="wepp-logo" alt="logo" width="64" height="64" />
+      <h2 id="wepp-name"></h2>
     </div>
-  `
+    <ol style="margin-block:4px;padding:0;margin-left:20px;">
+      <li>
+        ${TEXT.IOS.INSTALL_STEPS.STEP_1_1}
+        <span style="vertical-align:middle;">
+          <svg version="1.1" viewBox="0 0 2048 2048" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path transform="translate(600,655)" d="m0 0h301l1 2v79..."/>
+            <path transform="translate(1023,229)" d="m0 0 5 3 269 269..."/>
+          </svg>
+        </span>
+        ${TEXT.IOS.INSTALL_STEPS.STEP_1_2}
+      </li>
+      <li>
+        <p style="margin-block:4px;">${TEXT.IOS.INSTALL_STEPS.STEP_2}</p>
+      </li>
+    </ol>
+    <button id="wepp-skip-button">${TEXT.SKIP_BUTTON}</button>
+  </div>
+`;
+
+const DEFAULT_MODAL_CONTENT = `
+  <div class="wepp-modal-content" style="padding: 20px;">
+    <div class="wepp-modal-body">
+      <h1 style="margin-top: 0; margin-bottom: 10px;">${TEXT.TITLE}</h1>
+      <img id="wepp-logo" alt="logo" width="64" height="64" />
+      <h2 id="wepp-name"></h2>
+    </div>
+    <button id="wepp-install-button" style="margin-top: 10px;padding: 10px;" disabled>${TEXT.WAITING}</button>
+    <button id="wepp-skip-button" style="margin-top: 20px">${TEXT.SKIP_BUTTON}</button>
+  </div>
+`;
 
 // functions
 
-/**
- * Function that identifies the favicon as the default logo 
- * and gets the address
- */
 function getFaviconHref() {
   const linkElements = document.getElementsByTagName('link');
-
   for (let i = 0; i < linkElements.length; i++) {
     if (linkElements[i].getAttribute('rel') === 'icon') {
       return linkElements[i].getAttribute('href');
     }
   }
-
   return '';
 }
 
 function appendStyles() {
   const style = document.createElement('style');
   style.textContent = MODAL_STYLE;
-
   document.head.appendChild(style);
 }
 
-const getModalContent = (isIOS) => {
-  return isIOS ? IOS_MODAL_CONTENT : DEFAULT_MODAL_CONTENT;
-};
+const getModalContent = (isIOS) => isIOS ? IOS_MODAL_CONTENT : DEFAULT_MODAL_CONTENT;
 
 function createContainer() {
   const container = document.createElement('div');
@@ -134,7 +106,6 @@ function createContainer() {
 const handleHashChange = () => {
   const hash = window.location.hash;
   const isValidHash = hash.startsWith('#wepp-install-modal');
-
   const modal = document.getElementById('wepp-install-modal');
   modal.style.display = isValidHash ? 'flex' : 'none';
 };
@@ -142,13 +113,11 @@ const handleHashChange = () => {
 const initializePWAInfo = () => {
   const nameElement = document.getElementById('wepp-name');
   const logoElement = document.getElementById('wepp-logo');
-
   const name = document.title;
   const logo = getFaviconHref();
-
   nameElement.textContent = name;
   logoElement.src = logo;
-}
+};
 
 function handleModalClose() {
   window.location.hash = '';
@@ -164,85 +133,67 @@ function initializeModalEvents() {
   const modal = document.getElementById('wepp-install-modal');
   const skipButton = document.getElementById('wepp-skip-button');
   const modalContent = modal.querySelector('.wepp-modal-content');
-
-  // Close Modal on Background Click
   modal.addEventListener('click', handleModalClose);
   modalContent.addEventListener('click', preventModalContentClick);
-
   skipButton.addEventListener('click', handleModalClose);
 }
 
 const showPrompt = (deferredPrompt) => {
   try {
-    deferredPrompt.prompt(); // Show the install prompt
-    // Wait for the user to respond to the prompt
+    deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        // User accepted the install prompt
         handleModalClose();
-      } else {
-        // User dismissed the install prompt
       }
-      // Clear the deferredPrompt variable
       deferredPrompt = null;
     });
   } catch (error) {
     console.log('Error: ', error);
   }
-}
+};
 
 function main() {
   const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
   const isRunningAsPWA =
-    window.matchMedia('(display-mode: standalone)').matches
-    || window.navigator.standalone === true; // iOS
+    window.matchMedia('(display-mode: standalone)').matches ||
+    window.navigator.standalone === true;
 
   appendStyles();
-
-  // Create Modal containers and add them to dom
   const container = createContainer();
   container.innerHTML = getModalContent(isIOS);
-  
-
-  // initializeModal
   handleHashChange();
-
   initializePWAInfo();
-
   initializeModalEvents();
-
-  // initialize hash change event
   window.addEventListener('hashchange', handleHashChange);
 
-  if (isMobile && !isRunningAsPWA && !window.location.hash.includes('#wepp-install-modal')) {
-    window.location.hash = '#wepp-install-modal';
-  }  
-
-  // ----------------------------------------------------------
   if (isRunningAsPWA) {
     handleModalClose();
-  }
-  else if (!isIOS) {
-    let beforeInstallPromptFired = false;
-    const installButton = document.getElementById('wepp-install-button');
+  } else {
+    if (isMobile) {
+      window.location.hash = '#wepp-install-modal';
+    }
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      e.preventDefault();
-      const deferredPrompt = e;
-      installButton.addEventListener('click', () => showPrompt(deferredPrompt), { once: true })
-      installButton.innerText = TEXT.INSTALL_BUTTON;
-      installButton.disabled = false;
+    if (!isIOS) {
+      let beforeInstallPromptFired = false;
+      const installButton = document.getElementById('wepp-install-button');
 
-      beforeInstallPromptFired = true;
-    });
+      window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();
+        const deferredPrompt = e;
+        installButton.addEventListener('click', () => showPrompt(deferredPrompt), { once: true });
+        installButton.innerText = TEXT.INSTALL_BUTTON;
+        installButton.disabled = false;
+        beforeInstallPromptFired = true;
+      });
 
-    // Give the browser time to fire the beforeinstallprompt event
-    setTimeout(() => {
-      if (!beforeInstallPromptFired) {
-        installButton.innerText = TEXT.INSTALL_UNAVAILABLE;
-        installButton.disabled = true;
-      }
-    }, 1000); // 1 second delay
+      setTimeout(() => {
+        if (!beforeInstallPromptFired) {
+          installButton.innerText = TEXT.INSTALL_UNAVAILABLE;
+          installButton.disabled = true;
+        }
+      }, 1000);
+    }
   }
 
   window.addEventListener('appinstalled', () => {
@@ -251,9 +202,7 @@ function main() {
 }
 
 if (document.readyState === "loading") {
-  // Loading hasn't finished yet
   document.addEventListener("DOMContentLoaded", main);
 } else {
-  // `DOMContentLoaded` has already fired
   main();
 }
